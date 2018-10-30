57b193710f5cb8dd94a3018e733a4c954e9a2734
thewart <seth.madlonkay@gmail.com>
2018-10-30 19:59:29 -0400

    moved census parsing to other file and fixed which group column used

--- relationscript.R
+++ relationscript.R
@@ -1,7 +1,7 @@
-iskin <- function(data,kinmat,thresh=0.24) {
+iskin <- function(data,kinmat,thresh=0.5) {
   inds <- data[,cbind(match(FocalID,rownames(A)),
                 match(PartnerID,colnames(A)))]
-  return(A[inds]>thresh)
+  return(A[inds]>=thresh)
 }
 
 samesex <- function(data,demo) {
@@ -14,7 +14,7 @@ addmodifier <- function(data,func,modname=as.character(substitute(func)),...) {
                                  modname,"(",modcontent[!is.na(modcontent)],")")]
 }
 
-countkin <- function(ID,kinmat,thresh=0.25) {
+countkin <- function(ID,kinmat,thresh=0.5) {
   kinnames <- colnames(kinmat)
   tmp <- (kinmat>=thresh)[match(ID,kinnames),match(ID,kinnames)]
   return(data.table(ID=colnames(tmp),kincount=colSums(tmp)-1))
@@ -35,15 +35,4 @@ partnerrank <- function(data,rank) {
 
 
 
-censifiles <- paste0("~/Dropbox/Cayo Census/",c("2013/2013.June_animalels in cs-orig matriline June 2013.xls",
-                                                "2014/2014.AUG_animalels in cs-orig matriline August 2014.xls",
-                                                "2015/2015.June.animalels in cs-orig matriline June 2015.xls",
-                                                "2016/2016 April.animalels in cs-orig matriline abril 2016.xls"))
-censiyear <- c("2013","2014","2015","2016")
 
-censi <- lapply(censifiles,read_excel)
-for (i in 1:length(censi)) censi[[i]]$YEAR <- censiyear[i]
-censi <- do.call(rbind,censi) %>% as.data.table()
-censi <- censi[paste0(GROUP,YEAR) %in% bdat[,paste0(Group,Year)]]
-
-kincounts <- censi[str_length(animal_id2)==3,countkin(animal_id2,A,0.25),by=.(YEAR,GROUP)]
